92b0b2b951df12b3bd9424fc05b79a749e8cde28
battila98 <55810364+battila98@users.noreply.github.com>
2020-03-05 16:46:49 +0100

    5.feladatig kész

---
+++ Fantasycard/Fantasycard/Fantasycard/Card.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasycard
+{
+    class Card
+    {
+        private int power;
+        private string name;
+        private string faction;
+        private string race;
+        private string class_;
+
+        public Card(int power, string name, string faction, string race, string class_)
+        {
+            this.power = power;
+            this.name = name;
+            this.faction = faction;
+            this.race = race;
+            this.class_ = class_;
+        }
+
+        public int Power { get => power; set => power = value; }
+        public string Name { get => name; set => name = value; }
+        public string Faction { get => faction; set => faction = value; }
+        public string Race { get => race; set => race = value; }
+        public string Class_ { get => class_; set => class_ = value; }
+
+        public override string ToString()
+        {
+            return String.Format("{0};{1};{2};{3};{4}", power, name, faction, race, class_);
+        }
+
+
+
+
+
+    }
+}
